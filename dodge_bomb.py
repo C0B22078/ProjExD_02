853b0c03b501a3b8d33371b8b99a12eab6ba4e18
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-20 15:22:44 +0900

    画面判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1500, 800
 delta = {
 pg.K_UP: (0,-5),
 pg.K_DOWN: (0,+5),
@@ -12,13 +12,28 @@ pg.K_LEFT: (-5,0),
 pg.K_RIGHT: (+5,0),
 }
 
+def window_judge(rct:pg.rect) -> tuple[bool,bool]:
+    """
+    こうかとんRect,爆弾Rectが画面外or 画面内かを判定する関数
+    引数:こうかとんRect or 爆弾Rect
+    戻り値;横方向,縦方向の判定結果タプル 
+    """
+
+
+    yoko,tate = True,True  #画面内であればTrue
+    if rct.left < 0 or WIDTH <rct.right :
+        yoko = False 
+    if rct.top < 0 or HEIGHT <rct.bottom:
+        tate = False
+    return yoko,tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")#backgroundimage
     kk_img = pg.image.load("ex02/fig/3.png")#koukaton_image
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    #こうかとんSurface (kk_img)から効果トンRect(kk_rct)を抽出する　
+    #こうかとんSurface (kk_img)からこうかとんRect(kk_rct)を抽出する　
     kk_rct =kk_img.get_rect()
     kk_rct.center=900,400
     bomb_img = pg.Surface((20,20))
@@ -40,6 +55,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0,0] #合計移動量
 
@@ -48,15 +64,26 @@ def main():
                 sum_mv[0]+= mv[0]
                 sum_mv[1]+= mv[1]
                 
-            kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        if window_judge(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bomb_rect.move_ip(vx,vy) #練習2
+        yoko,tate = window_judge(bomb_rect)
+        if not yoko: #横方向に画面外だったら
+            vx *= -1
+        if not tate : #縦方向に範囲外であれば
+            vy *= -1 
         screen.blit(bomb_img,bomb_rect)
+    
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+
+    
 
 
 if __name__ == "__main__":
