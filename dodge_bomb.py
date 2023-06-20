da04c180001b37cd0ca8236bc55280683813ddcb
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-20 15:23:16 +0900

    gameover

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -55,7 +55,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        
+        if kk_rct.colliderect(bomb_rect):
+            print("game over")
+            return  #ガメオベラ
         key_lst = pg.key.get_pressed()
         sum_mv = [0,0] #合計移動量
 
