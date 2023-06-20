9a0fe9b62bc8a600fe18c56c6d47438e321fbc49
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-20 14:13:49 +0900

    爆弾Surface

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
 import sys
+import random
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -11,6 +13,16 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")#backgroundimage
     kk_img = pg.image.load("ex02/fig/3.png")#koukaton_image
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bomb_img = pg.Surface((20,20))
+    bomb_img.set_colorkey((0,0,0)) #黒い部分を透明に
+    pg.draw.circle(bomb_img,(255,0,0),(10,10),10)
+    x = random.randint(0,WIDTH)
+    y = random.randint(0,HEIGHT)
+    #爆弾Surface(bomb_img)
+    bomb_rect = bomb_img.get_rect()
+    #bomb_rectの中心座標を乱数指定
+    bomb_rect.center = x,y 
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +32,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb_img,bomb_rect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
